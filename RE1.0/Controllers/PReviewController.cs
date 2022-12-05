a949f3a7ff312b9c8941f01fa3bf55b2daa24559
geetakb <geetakogale@gmail.com>
2022-12-05 11:05:24 +0530

    Add project files.

---
+++ RE1.0/Controllers/PReviewController.cs
@@ -0,0 +1,246 @@
+﻿
+using Newtonsoft.Json;
+using consumingWebAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Web;
+using System.Net;
+using System.Web.Mvc;
+
+namespace consumingWebAPI.Controllers
+{
+    public class PReviewController : Controller
+    {
+        // GET: ToDo
+        [HttpGet]
+        public ActionResult GetToDoList()
+        {
+            List<ToDo> TD = new List<ToDo>();
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://localhost:44328/api/");
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.AcceptLanguage.Add(new StringWithQualityHeaderValue("nl-NL"));
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+
+                var responseTask = client.GetAsync("ToDo/GetTaskList");
+                responseTask.Wait();
+
+                HttpResponseMessage response = responseTask.Result;
+                if (response.IsSuccessStatusCode)
+                {
+                    
+                    var readTask = response.Content.ReadAsAsync<List<ToDo>>();
+                    readTask.Wait();
+                    TD = readTask.Result;
+
+
+                }
+            }
+            return View(TD);
+        }
+        public ActionResult GetReviewList()
+        {
+            List<TaskReview> TR = new List<TaskReview>();
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://localhost:44328/api/");
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.AcceptLanguage.Add(new StringWithQualityHeaderValue("nl-NL"));
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+
+                var responseTask = client.GetAsync("Review/GetTaskReviewList");
+                responseTask.Wait();
+
+                HttpResponseMessage response = responseTask.Result;
+                if (response.IsSuccessStatusCode)
+                {
+                  
+                    var readTask = response.Content.ReadAsAsync<List<TaskReview>>();
+                    readTask.Wait();
+                    TR = readTask.Result;
+
+
+                }
+            }
+            return View(TR);
+        }
+
+
+
+        [HttpGet]
+        public ActionResult GetTaskReviewbytheirID(int ToDoId)
+        {
+            List<TaskReview> TR = new List<TaskReview>();
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://localhost:44328/api/");
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.AcceptLanguage.Add(new StringWithQualityHeaderValue("nl-NL"));
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+               
+                var responseTask = client.GetAsync("Review/GetTaskReviewbytheirID?ToDoId=" + ToDoId.ToString());
+               
+                responseTask.Wait();
+
+                HttpResponseMessage response = responseTask.Result;
+                if (response.IsSuccessStatusCode)
+                {
+                    // Get back a single task review object
+                    var readTask = response.Content.ReadAsAsync<List<TaskReview>>();
+                    readTask.Wait();
+                    TR = readTask.Result;
+                }
+            }
+            return View(TR);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create( TaskReview Tr)
+        {
+            if (ModelState.IsValid)
+            {
+                using (var client = new HttpClient())
+                {
+                    client.BaseAddress = new Uri("https://localhost:44328/api/Review");
+
+                    var response = await client.PostAsJsonAsync("Review/PostReview", Tr);
+                    if (response.IsSuccessStatusCode)
+                    {
+                        return RedirectToAction("GetTaskReviewbytheirID", new { ToDoId= Tr.ToDoId});
+                    }
+                    else
+                    {
+                        ModelState.AddModelError(string.Empty, "Server error try after some time.");
+                    }
+                }
+            }
+            return View(Tr);
+        }
+        public async Task<ActionResult> Edit(int ReviewID)
+        {
+            if (ReviewID == 0)
+            {
+                return View();
+            }
+            TaskReview Tr = null;
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://localhost:44328/api/Review/");
+
+                var result = await client.GetAsync("GetReviewById?ReviewID=" + ReviewID.ToString());
+
+                if (result.IsSuccessStatusCode)
+                {
+                    Tr = await result.Content.ReadAsAsync<TaskReview>();
+                }
+                else
+                {
+                    ModelState.AddModelError(string.Empty, "Server error try after some time.");
+                }
+            }
+            if (Tr == null)
+            {
+                return View();
+            }
+            return View(Tr);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit(TaskReview Tr)
+        {
+            if (ModelState.IsValid)
+            {
+                using (var client = new HttpClient())
+                {
+                    client.BaseAddress = new Uri("https://localhost:44328/api/Review/");
+                    var response = await client.PutAsJsonAsync("Put", Tr);
+                    if (response.IsSuccessStatusCode)
+                    {
+                        return RedirectToAction("GetTaskReviewbytheirID", new { ToDoId= Tr.ToDoId});
+                    }
+                    else
+                    {
+                        ModelState.AddModelError(string.Empty, "Server error try after some time.");
+                    }
+                }
+                return RedirectToAction("GetReviewList");
+            }
+            return View(Tr);
+        }
+
+        public async Task<ActionResult> Delete(string ReviewID)
+        {
+            if (ReviewID == null)
+            {
+                return View();
+            }
+            TaskReview Tr = null;
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://localhost:44328/api/Review/");
+
+                var result = await client.GetAsync("GetReviewById?ReviewID=" + ReviewID);
+
+                if (result.IsSuccessStatusCode)
+                {
+                    Tr = await result.Content.ReadAsAsync<TaskReview>();
+                }
+                else
+                {
+                    ModelState.AddModelError(string.Empty, "Server error try after some time.");
+                }
+            }
+
+            if (Tr == null)
+            {
+                return View();
+            }
+            return View(Tr);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(string ReviewID)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://localhost:44328/api/Review/");
+
+                var response = await client.DeleteAsync("DeleteReview/?ReviewID="+ ReviewID);
+                if (response.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("GetToDoList");
+                }
+                else
+                    ModelState.AddModelError(string.Empty, "Server error try after some time.");
+            }
+            return View();
+        }
+
+     
+    }
+}
+
+
+   
+
+    
---
+++ RE1.0/Models/PerformanceReview.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace consumingWebAPI.Models
+{
+    public class TaskReview
+    {
+        [Key]
+        public int ReviewId { get; set; }
+        [Range(0,5,ErrorMessage ="Rating should be between 0 and 5")]
+        public int Rating { get; set; }
+        public string Comment { get; set; }
+
+        public DateTime CreateDate { get; set; }
+
+        public DateTime UpdatedDate { get; set; }
+        public int ToDoId { get; set; }
+        public virtual ToDo TD { get; set; }
+
+    }
+
+    public class ToDo
+    {
+        public int ToDoId { get; set; }
+        public string Title { get; set; }
+        public string description { get; set; }
+        public string Category { get; set; }
+        public DateTime AssignedDate { get; set; }
+        public DateTime DueDate { get; set; }
+    }
+    public class Event
+    {
+        public int EventId { get; set; }
+        public string EventName { get; set; }
+        public string Description { get; set; }
+        public DateTime? EventDate { get; set; }
+        public int Duration { get; set; }
+    }
+    public class Task
+    {
+        //Scalar properties
+
+        public int Taskid { get; set; }
+        public string tittle { get; set; }
+        public string description { get; set; }
+        public string category { get; set; }
+        //public DateTime AssignedDate { get; set; }
+        //public DateTime? DueDate { get; set; }
+        public int Rating { get; set; }
+        public string Comment { get; set; }
+
+        //Navigation properties
+        //public int EmpID { get; set; }
+        //public ICollection<Employee> Emp { get; set; }
+    }
+    public class Skill
+    {
+        public int SkillId { get; set; }
+        public string SkillName { get; set; }
+        public int SkillRate { get; set; }
+        public DateTime CreateDate { get; set; }
+    }
+
+    public class User
+    {
+        public int UserID { get; set; }
+        public string UserName { get; set; }
+        public string UserEmail { get; set; }
+        public string UserPassword { get; set; }
+    }
+
+    public class Question
+    {
+        public int QuestionID { get; set; }
+        public string QuestionList { get; set; }
+        public int UserID { get; set; }
+        public virtual User user { get; set; }
+    }
+
+    public class Answer
+    {
+        public int AnswerID { get; set; }
+        public string Comment { get; set; }
+        public int UserID { get; set; }
+        public DateTime? CommentDate { get; set; }
+
+        public int QuestionID { get; set; }
+        public virtual Question question { get; set; }
+    }
+
+  
+
+    public class Cart
+    {
+        public int CartId { get; set; }
+        public string CartName { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime? DueDate { get; set; }
+        public int Rating { get; set; }
+    }
+
+    public class Item
+    {
+        public int ItemId { get; set; }
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public string Status { get; set; }
+
+        public int CartId { get; set; }
+        public virtual Cart cart { get; set; }
+    }
+   
+
+    public class Asset
+    {
+        public int AssetId { get; set; }
+        public string AssetName { get; set; }
+        public int AssetSku { get; set; }
+        public string Description { get; set; }
+        public DateTime? CreateDate { get; set; }
+    }
+    public class Employee
+    {
+        [Key]
+        public int EmpID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Department { get; set; }
+
+
+    }
+
+}
